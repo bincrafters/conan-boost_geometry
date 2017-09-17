e96d49d9c450c398e23b6ef6ba0f310f94d35ab7
Jerry Wiltse <jerrywiltse@gmail.com>
2017-09-17 11:32:25 -0400

    Updated version to 1.65.1

--- conanfile.py
+++ conanfile.py
@@ -2,42 +2,42 @@ from conans import ConanFile, tools, os
 
 class BoostGeometryConan(ConanFile):
     name = "Boost.Geometry"
-    version = "1.64.0"
+    version = "1.65.1"
     short_paths = True
     url = "https://github.com/bincrafters/conan-boost-geometry"
-    description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
+    description = "Please visit http://www.boost.org/doc/libs/1_65_1/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     lib_short_names = ["geometry"]
-    requires =  "Boost.Algorithm/1.64.0@bincrafters/testing", \
-                      "Boost.Array/1.64.0@bincrafters/testing", \
-                      "Boost.Assert/1.64.0@bincrafters/testing", \
-                      "Boost.Concept_Check/1.64.0@bincrafters/testing", \
-                      "Boost.Config/1.64.0@bincrafters/testing", \
-                      "Boost.Container/1.64.0@bincrafters/testing", \
-                      "Boost.Core/1.64.0@bincrafters/testing", \
-                      "Boost.Function_Types/1.64.0@bincrafters/testing", \
-                      "Boost.Fusion/1.64.0@bincrafters/testing", \
-                      "Boost.Integer/1.64.0@bincrafters/testing", \
-                      "Boost.Iterator/1.64.0@bincrafters/testing", \
-                      "Boost.Lexical_Cast/1.64.0@bincrafters/testing", \
-                      "Boost.Math/1.64.0@bincrafters/testing", \
-                      "Boost.Move/1.64.0@bincrafters/testing", \
-                      "Boost.Mpl/1.64.0@bincrafters/testing", \
-                      "Boost.Multiprecision/1.64.0@bincrafters/testing", \
-                      "Boost.Numeric_Conversion/1.64.0@bincrafters/testing", \
-                      "Boost.Polygon/1.64.0@bincrafters/testing", \
-                      "Boost.Qvm/1.64.0@bincrafters/testing", \
-                      "Boost.Range/1.64.0@bincrafters/testing", \
-                      "Boost.Rational/1.64.0@bincrafters/testing", \
-                      "Boost.Serialization/1.64.0@bincrafters/testing", \
-                      "Boost.Smart_Ptr/1.64.0@bincrafters/testing", \
-                      "Boost.Static_Assert/1.64.0@bincrafters/testing", \
-                      "Boost.Throw_Exception/1.64.0@bincrafters/testing", \
-                      "Boost.Tokenizer/1.64.0@bincrafters/testing", \
-                      "Boost.Tuple/1.64.0@bincrafters/testing", \
-                      "Boost.Type_Traits/1.64.0@bincrafters/testing", \
-                      "Boost.Utility/1.64.0@bincrafters/testing", \
-                      "Boost.Variant/1.64.0@bincrafters/testing"
+    requires =  "Boost.Algorithm/1.65.1@bincrafters/testing", \
+                      "Boost.Array/1.65.1@bincrafters/testing", \
+                      "Boost.Assert/1.65.1@bincrafters/testing", \
+                      "Boost.Concept_Check/1.65.1@bincrafters/testing", \
+                      "Boost.Config/1.65.1@bincrafters/testing", \
+                      "Boost.Container/1.65.1@bincrafters/testing", \
+                      "Boost.Core/1.65.1@bincrafters/testing", \
+                      "Boost.Function_Types/1.65.1@bincrafters/testing", \
+                      "Boost.Fusion/1.65.1@bincrafters/testing", \
+                      "Boost.Integer/1.65.1@bincrafters/testing", \
+                      "Boost.Iterator/1.65.1@bincrafters/testing", \
+                      "Boost.Lexical_Cast/1.65.1@bincrafters/testing", \
+                      "Boost.Math/1.65.1@bincrafters/testing", \
+                      "Boost.Move/1.65.1@bincrafters/testing", \
+                      "Boost.Mpl/1.65.1@bincrafters/testing", \
+                      "Boost.Multiprecision/1.65.1@bincrafters/testing", \
+                      "Boost.Numeric_Conversion/1.65.1@bincrafters/testing", \
+                      "Boost.Polygon/1.65.1@bincrafters/testing", \
+                      "Boost.Qvm/1.65.1@bincrafters/testing", \
+                      "Boost.Range/1.65.1@bincrafters/testing", \
+                      "Boost.Rational/1.65.1@bincrafters/testing", \
+                      "Boost.Serialization/1.65.1@bincrafters/testing", \
+                      "Boost.Smart_Ptr/1.65.1@bincrafters/testing", \
+                      "Boost.Static_Assert/1.65.1@bincrafters/testing", \
+                      "Boost.Throw_Exception/1.65.1@bincrafters/testing", \
+                      "Boost.Tokenizer/1.65.1@bincrafters/testing", \
+                      "Boost.Tuple/1.65.1@bincrafters/testing", \
+                      "Boost.Type_Traits/1.65.1@bincrafters/testing", \
+                      "Boost.Utility/1.65.1@bincrafters/testing", \
+                      "Boost.Variant/1.65.1@bincrafters/testing"
 
                       #algorithm9 array3 assert1 concept_check5 config0 container7 core2 function_types5 fusion5 integer3 iterator5 lexical_cast8 math8 move3 mpl5 multiprecision10 numeric~conversion6 polygon6 qvm6 range7 rational6 serialization11 smart_ptr4 static_assert1 throw_exception2 tokenizer6 tuple4 type_traits3 utility5 variant9
                       
