68162ad03f4869de9cfb53042260b4443c635a4b
Rene Rivera <grafikrobot@gmail.com>
2017-12-20 21:14:32 -0600

    Initial 1.66.0 package branche.

--- conanfile.py
+++ conanfile.py
@@ -3,39 +3,39 @@ from conans import ConanFile
 
 class BoostGeometryConan(ConanFile):
     name = "Boost.Geometry"
-    version = "1.65.1"
+    version = "1.66.0"
 
     requires = \
-        "Boost.Algorithm/1.65.1@bincrafters/testing", \
-        "Boost.Array/1.65.1@bincrafters/testing", \
-        "Boost.Assert/1.65.1@bincrafters/testing", \
-        "Boost.Concept_Check/1.65.1@bincrafters/testing", \
-        "Boost.Config/1.65.1@bincrafters/testing", \
-        "Boost.Container/1.65.1@bincrafters/testing", \
-        "Boost.Core/1.65.1@bincrafters/testing", \
-        "Boost.Function_Types/1.65.1@bincrafters/testing", \
-        "Boost.Fusion/1.65.1@bincrafters/testing", \
-        "Boost.Integer/1.65.1@bincrafters/testing", \
-        "Boost.Iterator/1.65.1@bincrafters/testing", \
-        "Boost.Lexical_Cast/1.65.1@bincrafters/testing", \
-        "Boost.Math/1.65.1@bincrafters/testing", \
-        "Boost.Move/1.65.1@bincrafters/testing", \
-        "Boost.Mpl/1.65.1@bincrafters/testing", \
-        "Boost.Multiprecision/1.65.1@bincrafters/testing", \
-        "Boost.Numeric_Conversion/1.65.1@bincrafters/testing", \
-        "Boost.Polygon/1.65.1@bincrafters/testing", \
-        "Boost.Qvm/1.65.1@bincrafters/testing", \
-        "Boost.Range/1.65.1@bincrafters/testing", \
-        "Boost.Rational/1.65.1@bincrafters/testing", \
-        "Boost.Serialization/1.65.1@bincrafters/testing", \
-        "Boost.Smart_Ptr/1.65.1@bincrafters/testing", \
-        "Boost.Static_Assert/1.65.1@bincrafters/testing", \
-        "Boost.Throw_Exception/1.65.1@bincrafters/testing", \
-        "Boost.Tokenizer/1.65.1@bincrafters/testing", \
-        "Boost.Tuple/1.65.1@bincrafters/testing", \
-        "Boost.Type_Traits/1.65.1@bincrafters/testing", \
-        "Boost.Utility/1.65.1@bincrafters/testing", \
-        "Boost.Variant/1.65.1@bincrafters/testing"
+        "Boost.Algorithm/1.66.0@bincrafters/testing", \
+        "Boost.Array/1.66.0@bincrafters/testing", \
+        "Boost.Assert/1.66.0@bincrafters/testing", \
+        "Boost.Concept_Check/1.66.0@bincrafters/testing", \
+        "Boost.Config/1.66.0@bincrafters/testing", \
+        "Boost.Container/1.66.0@bincrafters/testing", \
+        "Boost.Core/1.66.0@bincrafters/testing", \
+        "Boost.Function_Types/1.66.0@bincrafters/testing", \
+        "Boost.Fusion/1.66.0@bincrafters/testing", \
+        "Boost.Integer/1.66.0@bincrafters/testing", \
+        "Boost.Iterator/1.66.0@bincrafters/testing", \
+        "Boost.Lexical_Cast/1.66.0@bincrafters/testing", \
+        "Boost.Math/1.66.0@bincrafters/testing", \
+        "Boost.Move/1.66.0@bincrafters/testing", \
+        "Boost.Mpl/1.66.0@bincrafters/testing", \
+        "Boost.Multiprecision/1.66.0@bincrafters/testing", \
+        "Boost.Numeric_Conversion/1.66.0@bincrafters/testing", \
+        "Boost.Polygon/1.66.0@bincrafters/testing", \
+        "Boost.Qvm/1.66.0@bincrafters/testing", \
+        "Boost.Range/1.66.0@bincrafters/testing", \
+        "Boost.Rational/1.66.0@bincrafters/testing", \
+        "Boost.Serialization/1.66.0@bincrafters/testing", \
+        "Boost.Smart_Ptr/1.66.0@bincrafters/testing", \
+        "Boost.Static_Assert/1.66.0@bincrafters/testing", \
+        "Boost.Throw_Exception/1.66.0@bincrafters/testing", \
+        "Boost.Tokenizer/1.66.0@bincrafters/testing", \
+        "Boost.Tuple/1.66.0@bincrafters/testing", \
+        "Boost.Type_Traits/1.66.0@bincrafters/testing", \
+        "Boost.Utility/1.66.0@bincrafters/testing", \
+        "Boost.Variant/1.66.0@bincrafters/testing"
 
     lib_short_names = ["geometry"]
     is_header_only = True
@@ -43,9 +43,9 @@ class BoostGeometryConan(ConanFile):
     # BEGIN
 
     url = "https://github.com/bincrafters/conan-boost-geometry"
-    description = "Please visit http://www.boost.org/doc/libs/1_65_1"
+    description = "Please visit http://www.boost.org/doc/libs/1_66_0"
     license = "www.boost.org/users/license.html"
-    build_requires = "Boost.Generator/1.65.1@bincrafters/testing"
+    build_requires = "Boost.Generator/1.66.0@bincrafters/testing"
     short_paths = True
     exports = "boostgenerator.py"
 
