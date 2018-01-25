eaca670d4b0f8c2fdace5aa28ecafb81bab96373
Jerry W <jerrywiltse@gmail.com>
2018-01-25 01:12:42 -0500

    Refresh round 2

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,8 @@ class BoostGeometryConan(ConanFile):
     name = "boost_geometry"
     version = "1.66.0"
     url = "https://github.com/bincrafters/conan-boost_geometry"
-
+    author = "Bincrafters <bincrafters@gmail.com>"
+    exports = ["LICENSE.md"]
     lib_short_names = ["geometry"]
     is_header_only = True
 
