6ff453772cd533050a03cd2f10fcebdcc32d29b1
Rene Rivera <grafikrobot@gmail.com>
2018-01-29 09:53:50 -0600

    Merge.

--- conanfile.py
+++ conanfile.py
@@ -17,37 +17,37 @@ class BoostGeometryConan(ConanFile):
         self.info.header_only()
 
     requires = (
-        "boost_package_tools/1.66.0@bincrafters/stable",
-        "boost_algorithm/1.66.0@bincrafters/stable",
-        "boost_array/1.66.0@bincrafters/stable",
-        "boost_assert/1.66.0@bincrafters/stable",
-        "boost_concept_check/1.66.0@bincrafters/stable",
-        "boost_config/1.66.0@bincrafters/stable",
-        "boost_container/1.66.0@bincrafters/stable",
-        "boost_core/1.66.0@bincrafters/stable",
-        "boost_function_types/1.66.0@bincrafters/stable",
-        "boost_fusion/1.66.0@bincrafters/stable",
-        "boost_integer/1.66.0@bincrafters/stable",
-        "boost_iterator/1.66.0@bincrafters/stable",
-        "boost_lexical_cast/1.66.0@bincrafters/stable",
-        "boost_math/1.66.0@bincrafters/stable",
-        "boost_move/1.66.0@bincrafters/stable",
-        "boost_mpl/1.66.0@bincrafters/stable",
-        "boost_multiprecision/1.66.0@bincrafters/stable",
-        "boost_numeric_conversion/1.66.0@bincrafters/stable",
-        "boost_polygon/1.66.0@bincrafters/stable",
-        "boost_qvm/1.66.0@bincrafters/stable",
-        "boost_range/1.66.0@bincrafters/stable",
-        "boost_rational/1.66.0@bincrafters/stable",
-        "boost_serialization/1.66.0@bincrafters/stable",
-        "boost_smart_ptr/1.66.0@bincrafters/stable",
-        "boost_static_assert/1.66.0@bincrafters/stable",
-        "boost_throw_exception/1.66.0@bincrafters/stable",
-        "boost_tokenizer/1.66.0@bincrafters/stable",
-        "boost_tuple/1.66.0@bincrafters/stable",
-        "boost_type_traits/1.66.0@bincrafters/stable",
-        "boost_utility/1.66.0@bincrafters/stable",
-        "boost_variant/1.66.0@bincrafters/stable"
+        "boost_package_tools/1.66.0@bincrafters/testing",
+        "boost_algorithm/1.66.0@bincrafters/testing",
+        "boost_array/1.66.0@bincrafters/testing",
+        "boost_assert/1.66.0@bincrafters/testing",
+        "boost_concept_check/1.66.0@bincrafters/testing",
+        "boost_config/1.66.0@bincrafters/testing",
+        "boost_container/1.66.0@bincrafters/testing",
+        "boost_core/1.66.0@bincrafters/testing",
+        "boost_function_types/1.66.0@bincrafters/testing",
+        "boost_fusion/1.66.0@bincrafters/testing",
+        "boost_integer/1.66.0@bincrafters/testing",
+        "boost_iterator/1.66.0@bincrafters/testing",
+        "boost_lexical_cast/1.66.0@bincrafters/testing",
+        "boost_math/1.66.0@bincrafters/testing",
+        "boost_move/1.66.0@bincrafters/testing",
+        "boost_mpl/1.66.0@bincrafters/testing",
+        "boost_multiprecision/1.66.0@bincrafters/testing",
+        "boost_numeric_conversion/1.66.0@bincrafters/testing",
+        "boost_polygon/1.66.0@bincrafters/testing",
+        "boost_qvm/1.66.0@bincrafters/testing",
+        "boost_range/1.66.0@bincrafters/testing",
+        "boost_rational/1.66.0@bincrafters/testing",
+        "boost_serialization/1.66.0@bincrafters/testing",
+        "boost_smart_ptr/1.66.0@bincrafters/testing",
+        "boost_static_assert/1.66.0@bincrafters/testing",
+        "boost_throw_exception/1.66.0@bincrafters/testing",
+        "boost_tokenizer/1.66.0@bincrafters/testing",
+        "boost_tuple/1.66.0@bincrafters/testing",
+        "boost_type_traits/1.66.0@bincrafters/testing",
+        "boost_utility/1.66.0@bincrafters/testing",
+        "boost_variant/1.66.0@bincrafters/testing"
     )
 
     # BEGIN
@@ -55,7 +55,7 @@ class BoostGeometryConan(ConanFile):
     description = "Please visit http://www.boost.org/doc/libs/1_66_0"
     license = "BSL-1.0"
     short_paths = True
-    build_requires = "boost_generator/1.66.0@bincrafters/stable"
+    build_requires = "boost_generator/1.66.0@bincrafters/testing"
 
     def package_id(self):
         getattr(self, "package_id_additional", lambda:None)()
